d1b4953f1b1e73b5518c57ceeda34bba75f6413c
SimonLuca Merlante <simonluca.merlante@gmail.com>
2024-11-22 14:03:53 +0100

    added tanstack routing

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import viteReact from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [TanStackRouterVite(), viteReact()],
+});
